f27bdd67373ddab7498f1f602d95f606f821f097
Luka-Hanzek <luka.hanzek2@gmail.com>
2026-01-27 19:21:45 +0100

    Add script for getting data overview

---
+++ scripts/python/print_schemas.py
@@ -0,0 +1,34 @@
+import argparse
+from pathlib import Path
+
+import sedona_fer.bench.benchmark
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Print schemas of loaded datasets"
+    )
+
+    def validate(value):
+        if not Path(value).is_file():
+            raise argparse.ArgumentTypeError(f"Config file {value} does not exist")
+        return value
+    parser.add_argument(
+        "--config",
+        required=True,
+        help="Path to yaml configuration file",
+        type=validate
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    cli_args = parse_args()
+
+    benchmark = sedona_fer.bench.benchmark.SedonaBenchmark(cli_args.config)
+
+    benchmark.display_schemas()
+
+if __name__ == "__main__":
+    main()
